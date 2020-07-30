cdcfc1fe7f6778c8de37ff7a93ca176cb857a953
Sudam Mondal <64835411+sudammondal@users.noreply.github.com>
2020-07-30 09:56:09 +0600

    all file added

--- js/main.js
+++ js/main.js
@@ -1,49 +1,37 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 0) {
-            $(".header").addClass("fixedMenu");
-        } else {
-            $(".header").removeClass("fixedMenu");
-        }
-    });
-    
-});
-// $(document).ready(function () {
-//     $('.sssyour-class').slick({
-//         autoplay: true,
-//         arrows: false,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         slidesToShow: 4,
-//         dots: true,
-//         focusOnSelect: true,
-//     });
-// });
-$('.sssyour-class').slick({
-    slidesToScroll: 1,
+  $(window).scroll(function () {
+      if ($(window).scrollTop() > 0) {
+          $(".header").addClass("fixedMenu");
+      } else {
+          $(".header").removeClass("fixedMenu");
+      }
+      if ($(window).scrollTop() > 0) {
+        $(".header_err").addClass("fixedMenu");
+    } else {
+        $(".header_err").removeClass("fixedMenu");
+    }
+  });
+
+  $('.sssyour-class').slick({
     arrows: false,
     autoplay: true,
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
           dots: false,
         }
       },
       {
-        breakpoint: 737,
+        breakpoint: 767,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
           dots: false,
         }
       },
@@ -51,7 +39,6 @@ $('.sssyour-class').slick({
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
           dots: false,
         }
       },
@@ -59,13 +46,48 @@ $('.sssyour-class').slick({
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
           dots: false,
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
-                  
+  $('.main_slieda_ttstmonials').slick({
+    autoplay: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        }
+      }
+    ]
+  });
+})
